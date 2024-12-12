8e4fdfa792485f4acbcd76f17a0490ff8b6b5038
aleksandr-verikon <sashava07@gmail.com>
2024-12-12 19:46:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ async def welcome() -> str:
     return {"Главная страница"}
 
 @app.get("/user")
-async def user_predator(username: str, age: int) -> dict:
+async def user_predator(username: str, age: int) -> str:
     return {"User": username, "Age": age}
 
 @app.get("/user/admin")
@@ -15,5 +15,5 @@ async def welcome_admin() -> str:
     return {"Вы вошли как администратор"}
 
 @app.get("/user/{user_id}")
-async def welcome_user(user_id: int) -> dict:
+async def welcome_user(user_id: int) -> str:
     return {f"Вы вошли как пользователь № {user_id}"}
