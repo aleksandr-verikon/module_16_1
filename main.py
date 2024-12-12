d1f3e331408d50738b5edc2c7a6dcd74dfe66d1d
aleksandr-verikon <sashava07@gmail.com>
2024-12-12 20:01:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def welcome() -> dict:
-    return {"Главная страница"}
+async def welcome() -> str:
+    return "Главная страница"
 
 @app.get("/user")
 async def user_predator(username: str, age: int) -> dict:
     return {"User": username, "Age": age}
 
 @app.get("/user/admin")
-async def welcome_admin() -> dict:
-    return {"Вы вошли как администратор"}
+async def welcome_admin() -> str:
+    return "Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
 async def welcome_user(user_id: int) -> dict:
